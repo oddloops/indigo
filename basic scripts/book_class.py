e1ad07e9b5b35cfdc05260017cb5d52a6870b87d
Vinci Wu <vinciwu.vwu@gmail.com>
2025-03-23 16:41:45 -0400

    Update book_class to include subclass magazine

--- basic scripts/book_class.py
+++ basic scripts/book_class.py
@@ -1,15 +1,28 @@
 class book:
-  title=""
-  type="book"
-  author=""
-  page=""
+  def __init__(self, title="", author="", page=0):
+    self.title=title
+    self.type="book"
+    self.author=author
+    self.page=page
 
   def description(self):
-    desc = "%s is a %s written by %s. It has %.0f pages." % (self.title, self.type, self.author, self.page)
+    desc = f"{self.title} is a {self.type} written by {self.author}. It has {self.page} pages."
     return desc
 
-book1 = book()
-book1.title="How to lose."
-book1.author="Tom & Jerry"
-book1.page=500
+class Magazine(book):
+  def __init__(self, title, author, page, edition=0, volume=0, category=""):
+    super().__init__(title, author, page)
+    self.type="magazine"
+    self.edition=edition
+    self.volume=volume
+    self.category=category
+
+  def description(self):
+    desc = f"{self.title} is a {self.type} written by {self.author}. It has {self.page} pages. It is the {self.edition} edition, volume {self.volume}, category: {self.category}"
+    return desc
+
+book1 = book("How to lose.", "Tom & Jerry", 500)
 print(book1.description())
+
+magazine = Magazine(title="How to Win", author="Homer Simpson", page=10, edition=1, volume=1, category='DUFF')
+print(magazine.description())
